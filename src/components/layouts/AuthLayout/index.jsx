3d1a7f1d8ef70d8af1bace112c21b6932eb8747b
Eric manosalva <ramses.manosalva@gmail.com>
2022-03-27 12:01:03 -0500

    Feat: Add menu

--- src/components/layouts/AuthLayout/index.jsx
+++ src/components/layouts/AuthLayout/index.jsx
@@ -1,11 +1,42 @@
 import LogoImage from '../../atoms/LogoImage'
 import PropTypes from 'prop-types'
+import Menu from '../../organisms/Menu'
+
+import classes from './styles.module.css'
+
+const menu = [
+  {
+    title: 'Inicio',
+    id: 'home'
+  },
+  {
+    title: 'Servicios',
+    id: 'services'
+  },
+  {
+    title: 'Nuestro Equipo',
+    id: 'team'
+  },
+  {
+    title: 'Portafolio',
+    id: 'briefcase'
+  },
+  {
+    title: 'Contácto',
+    id: 'contact'
+  },
+  {
+    title: 'Términos y condiciones',
+    id: 'terms'
+  }
+]
 
 const AuthLayout = ({children}) => (
-  <>
+  <div className={classes.container}>
     <LogoImage/>
+    <Menu menu={menu} active="services"/>
     {children}
-  </>
+  </div>
 )
 
 AuthLayout.propTypes = {
---
+++ src/components/organisms/Menu/index.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types'
+import classes from './styles.module.css'
+import classNames from 'classnames'
+import {useState} from 'react'
+
+const Menu = ({menu, active}) => {
+  const [open, setOpen] = useState(true)
+
+  const handleToggleOpen = () => {
+    setOpen(v => !v)
+  }
+
+  return (
+    <>
+      <div className={classes.icon} onClick={handleToggleOpen}>
+        {
+          !open && (
+            <div className={classes.openIcon}/>
+          )
+        }
+        {
+          open && (
+            <i className="fa-solid fa-arrow-left"/>
+          )
+        }
+      </div>
+      <ul className={classNames(classes.container, {[classes.open]: open})}>
+        {
+          menu.map(item => (
+            <li key={item.id} className={classNames({[classes.active]: item.id === active})}>{item.title}</li>
+          ))
+        }
+      </ul>
+    </>
+  )
+}
+
+Menu.propTypes = {
+  menu: PropTypes.array.isRequired,
+  active: PropTypes.string
+}
+
+export default Menu
